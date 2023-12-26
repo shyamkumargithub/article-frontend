c212012c6070da6574c1802462664a472a873eee
shyam <shyamlaishram79@gmail.com>
2023-12-26 19:54:52 +0530

    ui enhancement

--- src/components/blog-interaction.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlogInteraction = () => {
-  return (
-    <div>BlogInteraction</div>
-  )
-}
-
-export default BlogInteraction
--- src/components/publish-form.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PublishForm = () => {
-  return (
-    <div>PublishForm</div>
-  )
-}
-
-export default PublishForm
